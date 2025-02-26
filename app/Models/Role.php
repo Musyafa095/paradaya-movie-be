e8e21b0628f8ecabd635926ef03661ade9b9bcb8
Musyafa095 <musyafa095@gmail.com>
2025-02-26 23:53:02 +0700

    feat: add model and migration for movie model

---
+++ app/Models/Role.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class Role extends Model
+{
+    use HasFactory, HasUuids;
+    protected $table = 'roles';
+    protected $fillable = ['name'];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            $model->id = (string) Str::uuid();
+        });
+    }
+    public function user()
+    {
+        return $this->hasMany(User::class, 'role_id');
+    }
+}
