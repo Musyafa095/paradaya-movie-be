b7ac4a8a2ab06134a409279d6141550e3e994389
Musyafa095 <musyafa095@gmail.com>
2025-02-27 08:44:47 +0700

    feat: Add all API routes in routes folder

--- routes/api.php
+++ routes/api.php
@@ -13,4 +13,24 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::prefix('v1')->group(function(){
+  Route::apiResource('/genre', CategoryController::class);
+  Route::apiResource('/movie', NewsController::class); 
+// middleware roleAdmin
+  Route::middleware('auth:api', 'admin')->group(function(){
+    Route::resource('role', RoleController::class);
+  });
+  //Auth
+  Route::prefix('auth')->group(function (){
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/me', [AuthController::class, 'currentuser'])->middleware('auth:api');
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
+    Route::post('/account_verification', [AuthController::class, 'verifikasi'])->middleware('auth:api');
+    Route::post('/generate_otp_code', [AuthController::class, 'generateOtp'])->middleware('auth:api');
+  })->middleware('api');
+  //middleware update profile
+  Route::post('/profile', [ProfileController::class, 'updateProfile'])->middleware(['auth:api', 'verifiedAccount']);
+  Route::post('/review', [CommentController::class, 'updateReview'])->middleware(['auth:api', 'verifiedAccount']);
+  });
 
